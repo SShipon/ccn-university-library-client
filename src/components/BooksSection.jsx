0906ad5668dc03680171e1ae20de7226cb01baf5
oxshipon1@gmail.com <oxshipon1@gmail.com>
2025-07-20 15:27:11 +0600

    update card ui change

--- src/components/BooksSection.jsx
+++ src/components/BooksSection.jsx
@@ -9,8 +9,8 @@ const BooksSection = () => {
   if (loading) return <p className="text-center p-4">Loading books...</p>;
 
   return (
-        <div className=" px-[8px] py-8 flex justify-center">
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-5 w-full max-w-6xl">
+         <div className="w-full max-w-7xl mx-auto px-3 sm:px-4 md:px-8 py-8">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
         {books.map((book, index) => (
           <BookCard key={index} book={book} />
         ))}

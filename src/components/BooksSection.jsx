67de55630b68ecad4c44e99b584501f238fcd569
oxshipon1@gmail.com <oxshipon1@gmail.com>
2025-07-20 15:22:04 +0600

    make ui change and update

--- src/components/BooksSection.jsx
+++ src/components/BooksSection.jsx
@@ -4,13 +4,13 @@ import BookCard from "./ BookCard";
 
 
 const BooksSection = () => {
-  const { books, loading } = useLimitedBooks(6);
+  const { books, loading } = useLimitedBooks(8);
 
   if (loading) return <p className="text-center p-4">Loading books...</p>;
 
   return (
-        <div className=" bg-gray-100 dark:bg-black px-[10px] py-10 flex justify-center">
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5 w-full max-w-5xl">
+        <div className=" px-[8px] py-8 flex justify-center">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-5 w-full max-w-6xl">
         {books.map((book, index) => (
           <BookCard key={index} book={book} />
         ))}
